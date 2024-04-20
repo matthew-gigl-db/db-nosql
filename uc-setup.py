7673f5eb80b04f613ed8b12922a2b98691de4430
matthew-gigl-db <matthew-gigl-db>
2024-04-20 21:04:13 +0000

    added some additional markdown comments

--- uc-setup.py
+++ uc-setup.py
@@ -2,7 +2,7 @@
 # MAGIC %md
 # MAGIC # Unity Catalog Set-Up 
 # MAGIC
-# MAGIC This notebook creates a catalog and schema that will use to create tables, plus a managed volume that will use to land the raw synthetic files that we'll generate from using Synthetic Health's Synthea package.  
+# MAGIC This notebook creates a catalog and schema that we'll use to create tables, plus a managed volume that will use to land the raw synthetic files that we'll generate from using Synthetic Health's Synthea package.  
 # MAGIC
 # MAGIC It's recommended to use a catalog name that you have read and write access to, or to create a new catalog (assuming that you have that permissoin in UC) to you'll be able to use to run the demo.  A brand new catalog is not required.  Please note that there is not a default setting for catalog, therefore you will need to supply a value in the Databricks widget.  You're welcome to create your own schema name in your catalog of choice.  The default schema name is "synthea" since we'll be generating synthetic files using the Synthea package.  You may need to change the schema name if more than one person in your organization is running this demo at the same time.  
 # MAGIC
@@ -11,12 +11,22 @@
 # COMMAND ----------
 
 # MAGIC %md 
-# MAGIC Create a catalog and a schema to house the synthethic patient data.  
+# MAGIC ### Create a catalog and a schema to house the synthethic patient data.  
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Databricks Widgets
 
 # COMMAND ----------
 
 dbutils.widgets.text(name = "catalog_name", defaultValue="", label="Catalog Name")
 dbutils.widgets.text(name = "schema_name", defaultValue="synthea", label="Schema Name")
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC #### Create and Assign Catalog to Use
 
 # COMMAND ----------
 
@@ -36,6 +46,11 @@ catalog_name
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC #### Create and Assign Schema in the Catalog to Use 
+
+# COMMAND ----------
+
 schema_name = dbutils.widgets.get("schema_name")
 schema_name
 
@@ -49,6 +64,11 @@ schema_name
 # MAGIC %sql
 # MAGIC use schema ${schema_name};
 # MAGIC select current_schema();
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Create the Volume 
 
 # COMMAND ----------
 
