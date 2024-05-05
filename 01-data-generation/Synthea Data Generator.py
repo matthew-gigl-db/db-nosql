fd698db9eba2cf170592f1da14fc4ddd44c446f8
matthew-gigl-db <matthew-gigl-db>
2024-05-05 22:36:37 +0000

    renamed folders and started the data generation notebook

---
+++ 01-data-generation/Synthea Data Generator.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC # Synthea Data Generator 
+# MAGIC ***
+# MAGIC
+# MAGIC Setup: 
+# MAGIC
+# MAGIC * A catalog, schema, and volumne setup with write permissions for the principal executing this notebook. 
+# MAGIC * A cluster with Java JDK version 17 set as the default.
+# MAGIC * The **synthea-with-dependencies.jar** available for the cluster to use. 
+# MAGIC * A preconfigured Synthea configuration file.  
+# MAGIC
+# MAGIC All of these requirements can be set up using the notebooks found in the 00-setup-notebooks folder of this repo, or from the original on Github:  [https://github.com/matthew-gigl-db/db-nosql](https://github.com/matthew-gigl-db/db-nosql)
+# MAGIC
+# MAGIC ***
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
