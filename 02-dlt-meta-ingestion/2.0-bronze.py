ab4ddd2586c8166fdc6870c68a434d246f73dceb
matthew-gigl-db <matthew-gigl-db>
2024-05-15 07:46:20 +0000

    changed from widgets to spark.conf for dlt input settings

--- 02-dlt-meta-ingestion/2.0-bronze.py
+++ 02-dlt-meta-ingestion/2.0-bronze.py
@@ -1,15 +1,25 @@
 # Databricks notebook source
+# used for active development, but not run during DLT execution, use DLT configurations instead
 dbutils.widgets.dropdown(name = "env_mode", defaultValue = "prd", choices = ["dev", "tst", "uat", "prd"], label = "Environment Mode")
 dbutils.widgets.text(name = "catalog_name", defaultValue="", label="Catalog Name")
 dbutils.widgets.text(name = "schema_name", defaultValue="synthea", label="Schema Name")
 dbutils.widgets.text(name = "volume_name", defaultValue="synthetic_files_raw", label="Volume Name")
 
+spark.conf.set("workflow_inputs.env_mode", dbutils.widgets.get(name = "env_mode"))
+spark.conf.set("workflow_inputs.catalog_name", dbutils.widgets.get(name = "catalog_name"))
+spark.conf.set("workflow_inputs.schema_name", dbutils.widgets.get(name = "schema_name"))
+spark.conf.set("workflow_inputs.volume_name", dbutils.widgets.get(name = "volume_name"))
+
 # COMMAND ----------
 
-env_mode = dbutils.widgets.get(name = "env_mode")
-catalog_name = dbutils.widgets.get(name = "catalog_name")
-schema_name = dbutils.widgets.get(name = "schema_name")
-volume_name = dbutils.widgets.get(name = "volume_name")
+dbutils.widgets.removeAll()
+
+# COMMAND ----------
+
+env_mode = spark.conf.get("workflow_inputs.env_mode")
+catalog_name = spark.conf.get("workflow_inputs.catalog_name")
+schema_name = spark.conf.get("workflow_inputs.schema_name")
+volume_name = spark.conf.get("workflow_inputs.volume_name")
 volume_path = f"/Volumes/{catalog_name}/{schema_name}/{volume_name}/"
 print(f"""
     env_mode = {env_mode}
@@ -158,13 +168,13 @@ Pipeline
 
 # COMMAND ----------
 
-display(spark.sql("select current_catalog(), current_schema()"))
+# display(spark.sql("select current_catalog(), current_schema()"))
 
 # COMMAND ----------
 
 Pipeline.ingest_raw_to_bronze(
     table_name="synthea_csv_bronze"
     ,table_comment="A full text record of every file that has landed in our raw synthea landing folder."
-    ,table_properties={"quality":"bronze"}
+    ,table_properties=None #{"quality":"bronze"}
     ,source_folder_path_from_volume="output/csv"
 )
